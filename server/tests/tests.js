0bd30cedd8a6c659be718917dd5642b516f5d141
WillyWunderdog <odemichaelgbenga@gmail.com>
2017-07-13 12:39:06 +0100

    write test

--- server/tests/tests.js
+++ server/tests/tests.js
@@ -1,7 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../app';
-import models from '../model';
+import models from '../models';
 
 process.env.NODE_ENV = 'test';
 const should = chai.should();
@@ -9,24 +9,40 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 models.Users.destroy({
-    where: {},
-    cascade: true,
-    truncate: true
+  where: {},
+  cascade: true,
+  truncate: true
 });
 
 models.Groups.destroy({
-    where: {},
-    cascade: true,
-    truncate: true
+  where: {},
+  cascade: true,
+  truncate: true
 });
 
 models.Messages.destroy({
-    where: {},
-    cascade: true,
-    truncate: true
+  where: {},
+  cascade: true,
+  truncate: true
 });
 
-describe('', () => {
-    
-})
+describe('test app', () => {
+  describe('create user: ', () => {
+    it('POST /api/user/signup creates a new user', (done) => {
+      chai.request(app)
+        .post('/api/user/signup')
+        .type('form')
+        .send({
+          password: 'testpassword',
+          username: 'testusername',
+          email: 'test@user.com'
+        })
+        .end((err, res) => {
+          res.body.email.should.equal('test@user.com');
+          res.should.have.status(201);
+          done();
+        });
+    });
+  });
+});
 
